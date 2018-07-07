7b8f3e4f2dbfea69d6d457ca63f9d55d52d8f4a5
郭轩奇 <1049137461@qq.com>
2018-07-07 14:20:12 +0800

    学生成绩页面代码补充2

--- student/StudentMainForm.Designer.cs
+++ student/StudentMainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace student
 {
-    partial class Form1
+    partial class StudentMainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(190, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(758, 206);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "学号";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "姓名";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "性别";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "年龄";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "院系";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "课程";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "成绩";
+            this.Column7.Name = "Column7";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(-88, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "查看个人信息";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(-88, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "查询选修课程";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(-88, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "查询课程成绩";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(-88, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "选修新的课程";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // StudentMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Name = "StudentMainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
--- student/StudentMainForm.cs
+++ student/StudentMainForm.cs
@@ -10,11 +10,60 @@ using System.Windows.Forms;
 
 namespace student
 {
-    public partial class Form1 : Form
+    public partial class StudentMainForm : Form
     {
-        public Form1()
+        public string Sno;
+        public StudentMainForm(string str)
         {
+            this.Sno = str;
             InitializeComponent();
         }
-    }
-}
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = "server=111.231.69.100;port=3306;uid=linge;password=123456;Database=linge";
+            string cmdstr = string.Format("select Cname,Grade from SC,Course where SC.Sno='{0}' AND SC.Cno=Course.Cno", Sno);
+
+
+            MySqlConnection mycon = new MySqlConnection(constr);
+            mycon.Open();
+            MySqlCommand cmd = new MySqlCommand(cmdstr, mycon);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            object[] value = new object[10];
+            this.dataGridView1.Rows.Clear();
+            while (reader.Read())
+            {
+                int index = this.dataGridView1.Rows.Add();
+                for (int i = 0; i < reader.GetValues(value); i++)
+                {
+                    this.dataGridView1.Rows[index].Cells[i + 5].Value = reader[i].ToString();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string constr = "server=111.231.69.100;port=3306;uid=linge;password=123456;Database=linge";
+            string cmdstr = string.Format("select * from Student where Student.Sno='{0}'", Sno);
+
+
+            MySqlConnection mycon = new MySqlConnection(constr);
+            mycon.Open();
+
+            MySqlCommand cmd = new MySqlCommand(cmdstr, mycon);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            object[] value = new object[10];
+            this.dataGridView1.Rows.Clear();
+            while (reader.Read())
+            {
+                int index = this.dataGridView1.Rows.Add();
+                for (int i = 0; i < reader.GetValues(value); i++)
+                {
+                    this.dataGridView1.Rows[index].Cells[i].Value = reader[i].ToString();
+                }
+            }
+        }

7e4674764c72a473fe2200132cc5ddaa8cafeea0
郭轩奇 <1049137461@qq.com>
2018-07-07 14:24:38 +0800

    添加学生课程中显示之前修读的课程

--- student/SelectNewCourse.cs
+++ student/SelectNewCourse.cs
@@ -12,9 +12,51 @@ namespace student
 {
     public partial class SelectNewCourse : Form
     {
-        public SelectNewCourse()
+        public string Sno;
+        public SelectNewCourse(string str)
         {
+            this.Sno = str;
             InitializeComponent();
         }
-    }
-}
+
+        private void SelectNewCourse_Load(object sender, EventArgs e)
+        {
+            string constr = "server=111.231.69.100;port=3306;uid=linge;password=123456;Database=linge";
+            string cmdstr = string.Format("select Course.Cno,Cname from SC,Course where SC.Sno='{0}' AND SC.Cno=Course.Cno", Sno);
+            string cmdstr2 = string.Format("select Cno,Cname,Cpno from Course where not exists(select 1 from SC where SC.Cno=Course.Cno AND SC.Sno='{0}')", Sno);
+
+            MySqlConnection mycon = new MySqlConnection(constr);
+            MySqlConnection mycon1 = new MySqlConnection(constr);
+            mycon1.Open();
+            mycon.Open();
+
+            MySqlCommand cmd = new MySqlCommand(cmdstr, mycon);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            MySqlCommand cmd1 = new MySqlCommand(cmdstr2, mycon1);
+            MySqlDataReader reader1 = cmd1.ExecuteReader();
+
+            object[] value = new object[10];
+            this.dataGridView1.Rows.Clear();
+            while (reader.Read())
+            {
+                int index = this.dataGridView1.Rows.Add();
+                for (int i = 0; i < reader.GetValues(value); i++)
+                {
+                    this.dataGridView1.Rows[index].Cells[i].Value = reader[i].ToString();
+                }
+            }
+
+            object[] value1 = new object[10];
+            this.dataGridView2.Rows.Clear();
+            while (reader1.Read())
+            {
+                int index1 = this.dataGridView2.Rows.Add();
+                for (int i = 0; i < reader1.GetValues(value1); i++)
+                {
+                    this.dataGridView2.Rows[index1].Cells[i].Value = reader1[i].ToString();
+                }
+            }
+            mycon.Close();
+            mycon1.Close();
+        }

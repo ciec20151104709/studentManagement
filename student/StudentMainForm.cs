84ad2fe436bfb8c53c530594ae3bf5b01e65ac05
郭轩奇 <1049137461@qq.com>
2018-07-08 14:47:34 +0800

    程序最终完善

--- student/StudentMainForm.cs
+++ student/StudentMainForm.cs
@@ -67,3 +67,40 @@ namespace student
                 }
             }
         }
+        private void StudentMainForm_Load(object sender, EventArgs e)
+        {
+            this.Text = "欢迎你、学号为:  " + Sno + "  的同学";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = "server=111.231.69.100;port=3306;uid=linge;password=123456;Database=linge";
+            string cmdstr = string.Format("select Cname from SC,Course where SC.Sno='{0}' AND SC.Cno=Course.Cno", Sno);
+
+
+            MySqlConnection mycon = new MySqlConnection(constr);
+            mycon.Open();
+
+            MySqlCommand cmd = new MySqlCommand(cmdstr, mycon);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            object[] value = new object[10];
+            this.dataGridView1.Rows.Clear();
+            while (reader.Read())
+            {
+                int index = this.dataGridView1.Rows.Add();
+                for (int i = 0; i < reader.GetValues(value); i++)
+                {
+                    this.dataGridView1.Rows[index].Cells[i + 5].Value = reader[i].ToString();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SelectNewCourse S1 = new SelectNewCourse(this.Sno);
+            S1.Show();
+        }
+    }
+}
+
